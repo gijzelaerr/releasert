c17796cabc7581b986dfd3176c9dcd5ba4946f10
Gijs Molenaar <gijs@pythonic.nl>
2014-10-06 17:48:43 +0200

    first commit

---
+++ release.sh
@@ -0,0 +1,74 @@
+#!bash -ve
+#
+# this script will build a debian package from a github repo.
+# it is assumed that:
+#
+# * git and hub are installed
+#
+# * the git repo has a submodule called
+#   debian, which contains debian package files. 
+#
+# * you can access and commit to the git repo
+#
+# * you have access to dput to launchpad
+
+ORGANISATION=ska-sa
+WORKDIR=~/packaging
+DIST=trusty
+
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 <project name> <version>"
+    echo 
+    echo "example: $0 tigger 1.3.2"
+    echo 
+    exit 1
+fi
+
+NAME=$1
+VERSION=$2
+REPO=git@github.com:$ORGANISATION/$NAME.git
+DEB_REPO=git@github.com:$ORGANISATION/$NAME-debian.git
+RELEASE_URL=https://github.com/$ORGANISATION/$NAME/releases/tag/$VERSION
+
+if [ ! -x $WORKDIR ]; then
+    echo "$WORKDIR doens't exists, creating"
+    mkdir $WORKDIR
+fi
+
+cd $WORKDIR
+
+if [ ! -x $NAME ]; then
+    echo "checking out $NAME for the first time"
+    git clone $REPO $NAME
+    cd $NAME
+else
+    cd $NAME
+    git pull
+    git checkout master
+fi
+
+echo "* making new version"
+git tag $VERSION
+git push --tags
+echo "* release online now: RELEASE_URL"
+git archive --prefix=$NAME-$VERSION/ $VERSION | bzip2 >../${NAME}_$VERSION.orig.tar.bz2
+cd ..
+tar jxvf ${NAME}_$VERSION.orig.tar.bz2
+cd $NAME-$VERSION 
+
+echo "* preparing debian files"
+git clone $DEB_REPO debian
+cd debian
+git checkout $DIST
+dch -v $VERSION-1$DIST -D $DIST --force-distribution "new upstream release" || true
+git commit changelog -m "new upstream release"
+git push
+
+echo "* building source package"
+cd ..
+debuild -S -sa
+
+echo "* pushing to launchpad PPA"
+dput ppa:ska-sa/main ../${NAME}_$VERSION-1$DIST_source.changes
+
